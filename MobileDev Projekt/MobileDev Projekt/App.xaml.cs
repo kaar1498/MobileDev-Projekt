e43b0e8ed5e7089a1388f9686e62541f47242576
Ibsylong <luanbanguyen94@gmail.com>
2021-04-14 13:15:53 +0200

    Homepage

--- MobileDev Projekt/MobileDev Projekt/App.xaml.cs
+++ MobileDev Projekt/MobileDev Projekt/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MobileDev_Projekt.Pages;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -10,7 +11,7 @@ namespace MobileDev_Projekt
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new HomePage();
         }
 
         protected override void OnStart()
